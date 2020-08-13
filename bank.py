0993f972f7f1b2e69cd5a55e8c89f7f536a1f98d
Likai Kuroi <likai31197@gmail.com>
2020-08-13 08:32:28 -0400

    simple bank system w/ commands

--- bank.py
+++ bank.py
@@ -3,44 +3,84 @@ Keeps track of a number value for a given discord user, modified through command
 '''
 import os
 import json
+import discord 
+import operator
 
 class Bank:
     def __init__(self, bank_file="bank.log"):
-        file_to_open = bank_file
-        dir = os.path.dirname(__file__) # absolute dir the script is running in 
-        if os.path.getsize(os.path.join(dir, file_to_open)) > 0:
-            bank_file = open(os.path.join(dir, file_to_open), "w+")
-            bank = json.loads(bank_file.read()) # Json to Dictionary
+        self.bank = {}
+        self.file_to_open = bank_file
+        self.dir = os.path.dirname(__file__) # absolute dir the script is running in 
+        self.initial_bank_load()
+        self.money_sign = "$"
+
+    def initial_bank_load(self):
+        if os.path.getsize(os.path.join(self.dir, self.file_to_open)) > 0: # if there's data in there
+            bank_file = open(os.path.join(self.dir, self.file_to_open), "r")
+            self.bank = json.loads(bank_file.read()) # Json to Dictionary
+            bank_file.flush()
+            bank_file.close()
+            print(self.bank)
         else:
-            bank = {} # if there's no file already, make a new bank.
+            self.bank = {}
+        
+
+    def write_to_file(self):
+        bank_file = open(os.path.join(self.dir, self.file_to_open), "w")
+        bank_file.write(json.dumps(self.bank)) # not very efficient
+        bank_file.flush()
+        os.fsync(bank_file.fileno())
+        bank_file.close()
 
     def increment(self, userid, amt):
         if not isinstance(amt, int):
             raise Exception("Incorrect type for amt") 
-        elif not isinstance(userid, int):
-            raise Exception("Incorrect type for userid") 
-        if userid in bank:
-            bank[userid] += amt
+
+        if userid in self.bank:
+            self.bank[userid] += amt
         else:
-            bank[userid] = amt
-        bank_file.write(json.dumps(bank)) # not very efficient
+            self.bank[userid] = amt
+        self.write_to_file()
     
     def decrement(self, userid, amt):
         if not isinstance(amt, int):
             raise Exception("Incorrect type for amt") 
-        elif not isinstance(userid, int):
-            raise Exception("Incorrect type for userid") 
-        if userid in bank:
-            bank[userid] -= amt
+        if userid in self.bank:
+            self.bank[userid] -= amt
         else:
-            bank[userid] = -amt
-        bank_file.write(json.dumps(bank)) # not very efficient
+            self.bank[userid] = -amt
+        self.write_to_file()
 
     def set_to(self, userid, amt):
         if not isinstance(amt, int):
             raise Exception("Incorrect type for amt") 
-        elif not isinstance(userid, int):
-            raise Exception("Incorrect type for userid") 
-            bank[userid] = amt
+        self.bank[userid] = amt
+        self.write_to_file()
 
-        bank_file.write(json.dumps(bank)) # not very efficient
+    def get_balance(self, userid):
+        if userid in self.bank:
+            return self.bank[userid]
+    
+    def remove_id(self, userid):
+        if userid in self.bank:
+            self.bank.pop(userid)
+
+
+
+    def all_balances(self, client):
+        
+        output = ""
+        # https://careerkarma.com/blog/python-sort-a-dictionary-by-value/#:~:text=To%20sort%20a%20dictionary%20by%20value%20in%20Python%20you%20can,Dictionaries%20are%20unordered%20data%20structures.
+        balances = sorted(self.bank.items(), key=lambda x: x[1], reverse=True)
+
+        for sorted_users in balances:
+            username = client.get_user(int(sorted_users[0])).name
+            if balances.index(sorted_users) == 0:
+                output += ("```diff\n- " + username + "\t" + self.money_sign + str(sorted_users[1]) + "\n```") # makes it red (the -)
+            elif balances.index(sorted_users) == 1:
+                output += ("```fix\n " + username + "\t" + self.money_sign + str(sorted_users[1]) + "\n```") # makes it yellow
+            elif balances.index(sorted_users) == 2:
+                output += ("```fix\n " + username + "\t" + self.money_sign + str(sorted_users[1]) + "\n```") # makes it yellow
+            else:
+                output += ("```diff\n+ " + username + "\t" + self.money_sign + str(sorted_users[1]) + "\n```") # makes it green
+        return output
--- bot.py
+++ bot.py
@@ -1,8 +1,31 @@
 import discord
+import random
 from bank import Bank
 
 client = discord.Client()
 bank = Bank()
+delimiter = "$"
+
+def get_error_response():
+    responses = [
+        "come again?",
+        "what",
+        "?",
+        "??",
+        "excuse me",
+        "uhh"
+    ]
+    return responses[random.randint(0, len(responses) - 1)]
+
+def get_user_id_from_message(msg):
+    output = ""
+    for char in msg:
+        if char.isnumeric():
+            output = output + char
+    print(output)
+    return int(output)
+
+
 # https://discordpy.readthedocs.io/en/latest/quickstart.html
 @client.event
 async def on_ready():
@@ -10,10 +33,82 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    args = message.content.split()
     if message.author == client.user:
         return
 
-    if "hello" in message.content:
-        await message.channel.send("sup")
+    '''
+    Bank Section
 
+    Commands:
+    bill / add @userid amount
+    deduct / remove @userid amount
+    setbalance @userid amount
+    mybalance
+    balance @userid
+    balances
+
+    '''
+    if (args[0].startswith(delimiter+"bill") or args[0].startswith(delimiter+"add")) and len(args) >= 3:
+        user = get_user_id_from_message(args[1])
+        amt = int(args[2])
+        try:
+            if(client.get_user(user) != None): # confirm the user is actually existing
+                bank.increment(str(user), amt)
+                await message.add_reaction("\U0001F4B8")
+        except Exception as e:
+            print("error: ", str(e))
+            await message.channel.send(get_error_response())
+            return
+        return
+    elif (args[0].startswith(delimiter+"deduct") or args[0].startswith(delimiter+"remove")) and len(args) >= 3:
+        user = get_user_id_from_message(args[1])
+        amt = int(args[2])
+        try:
+            if(client.get_user(user) != None): # confirm the user is actually existing
+                bank.decrement(str(user), amt)
+                await message.add_reaction("\U0001F4B8")
+        except Exception as e:
+            print("error: ", str(e))
+            await message.channel.send(get_error_response())
+            return
+        return
+    elif args[0].startswith(delimiter+"setbalance") and len(args) >= 3:
+        user = get_user_id_from_message(args[1])
+        amt = int(args[2])
+        try:
+            if(client.get_user(user) != None): # confirm the user is actually existing
+                bank.set_to(str(user), amt)
+                await message.add_reaction("\U0001F4B8")
+        except Exception as e:
+            print("error: ", str(e))
+            await message.channel.send(get_error_response())
+            return
+        return       
+    elif args[0].startswith(delimiter+"mybalance"):
+        user = message.author.id
+        try:
+            await message.channel.send(client.get_user(user).display_name+ "\'s balance is " + str(bank.get_balance(str(user))))
+        except Exception as e:
+            print("error: ", str(e))
+            await message.channel.send(get_error_response())
+            return
+        return
+    elif args[0].startswith(delimiter+"balance") and len(args) == 2:
+        user = get_user_id_from_message(args[1])
+        try:
+            await message.channel.send(client.get_user(user).display_name + "\'s balance is " + str(bank.get_balance(str(user))))
+        except Exception as e:
+            print("error: ", str(e))
+            await message.channel.send(get_error_response())
+            return
+        return
+    elif args[0].startswith(delimiter+"balances"):
+        try:
+            await message.channel.send(bank.all_balances(client))
+        except Exception as e:
+            print("error: ", str(e))
+            await message.channel.send(get_error_response())
+            return
+        return
 client.run('')
